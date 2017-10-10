e3bcfe7838a38fd09b893abd7f1afb9f71653236
Rohan Balakrishnan <rohanbk@gmail.com>
2017-10-09 21:11:24 -0700

    Refactor Spider properties and modify link extraction logic

--- mountain_project_scraper/spiders/coordinates_spider.py
+++ mountain_project_scraper/spiders/coordinates_spider.py
@@ -1,18 +1,33 @@
 import scrapy
-import json
+import re
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 class CoordinatesSpider(scrapy.Spider):
     name = 'coordinates'
+    start_urls = ['https://www.mountainproject.com/v/kansas/107235316'] #replace this with mountain project homepage
+    allowed_domains = ['mountainproject.com']
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow='/v/(.+)/'),
+            callback='parse_coordinates'
+        )
+    ]
 
-    def start_requests(self):
-        urls = [
-            'https://www.mountainproject.com/v/kansas/107235316'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    def parse_coordinates(self, response):
+        return {
+            'sidebar_links': response.css('#viewerLeftNavColContent a[target="_top"]').extract(),
+            'coordinates': self.process_gps_coordinates(response)
+        }
 
-    def parse(self, response):
-        sidebar_links = response.css('#viewerLeftNavColContent a[target="_top"]').extract()
-        lat_long_dirty = response.css('#rspCol800 div.rspCol table tr').extract()[1] #find out how to process this and extract lat/long
+    # helper function to take HTML response object and product object contains latitude and longitude
+    def process_gps_coordinates(self, response):
+        # produce string in format 'XXX.XXX,XXX.XXX'
+        lat_long_string = re.search(r"q=[^&]*", response.css('#rspCol800 div.rspCol table tr:nth-child(2) td').extract()[1]).group(0)[2:]
+        return {
+            'latitude' : lat_long_string.split()[0],
+            'longitude' : lat_long_string.split()[1]
+        }
 
 
