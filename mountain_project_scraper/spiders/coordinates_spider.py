7b10617917d8f423c5d265fff413821051302d06
Rohan Balakrishnan <rohanbk@gmail.com>
2017-10-10 19:32:39 -0700

    Add new field and selectors to capture Area Name

--- mountain_project_scraper/spiders/coordinates_spider.py
+++ mountain_project_scraper/spiders/coordinates_spider.py
@@ -3,6 +3,7 @@ import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+
 class CoordinatesSpider(scrapy.Spider):
     name = 'coordinates'
     start_urls = ['https://www.mountainproject.com/v/kansas/107235316'] #replace this with mountain project homepage
@@ -18,13 +19,15 @@ class CoordinatesSpider(scrapy.Spider):
     def parse_coordinates(self, response):
         return {
             'sidebar_links': response.css('#viewerLeftNavColContent a[target="_top"]').extract(),
+            'area_name': re.search(r"<em>[A-Za-z]*",response.css('h1.dkorange em').extract()[0]).group(0)[4:],
             'coordinates': self.process_gps_coordinates(response)
         }
 
     # helper function to take HTML response object and product object contains latitude and longitude
     def process_gps_coordinates(self, response):
         # produce string in format 'XXX.XXX,XXX.XXX'
-        lat_long_string = re.search(r"q=[^&]*", response.css('#rspCol800 div.rspCol table tr:nth-child(2) td').extract()[1]).group(0)[2:]
+        lat_long_markup = response.css('#rspCol800 div.rspCol table tr:nth-child(2) td').extract()[1]
+        lat_long_string = re.search(r"q=[^&]*", lat_long_markup).group(0)[2:]
         return {
             'latitude' : lat_long_string.split()[0],
             'longitude' : lat_long_string.split()[1]
