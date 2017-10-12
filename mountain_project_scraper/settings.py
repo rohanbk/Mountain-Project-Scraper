4e6e2f30f47b0e2bb5a6240c8ab1576e75c1eabd
Rohan Balakrishnan <rohanbk@gmail.com>
2017-10-11 20:44:12 -0700

    MVP version of Scraper working
    Scraper capable of grabbing area-name + lat/long from area page

--- mountain_project_scraper/settings.py
+++ mountain_project_scraper/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'mountain_project_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'mountain_project_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- mountain_project_scraper/spiders/coordinates_spider.py
+++ mountain_project_scraper/spiders/coordinates_spider.py
@@ -6,31 +6,42 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class CoordinatesSpider(scrapy.Spider):
     name = 'coordinates'
-    start_urls = ['https://www.mountainproject.com/v/kansas/107235316'] #replace this with mountain project homepage
+    domain = 'https://www.mountainproject.com'
+    start_urls = [domain + '/v/washington/105708966'] #replace this with mountain project homepage
     allowed_domains = ['mountainproject.com']
     rules = [
         Rule(
-            LinkExtractor(
-                allow='/v/(.+)/'),
-            callback='parse_coordinates'
+            LinkExtractor(allow='v/(.+)'),
+            callback='parse',
+            follow=True
         )
     ]
 
+    def parse(self, response):
+        links = response.css('#viewerLeftNavColContent a[target="_top"] ::attr(href)').extract()
+        for url in links:
+            yield scrapy.Request(self.domain + url, callback=self.parse_coordinates)
+
     def parse_coordinates(self, response):
-        return {
-            'sidebar_links': response.css('#viewerLeftNavColContent a[target="_top"]').extract(),
-            'area_name': re.search(r"<em>[A-Za-z]*",response.css('h1.dkorange em').extract()[0]).group(0)[4:],
-            'coordinates': self.process_gps_coordinates(response)
-        }
+        area_name = response.css('h1.dkorange em ::text').extract_first().strip()
+        lat_long_string = self.process_latitude_longitude(response)
 
-    # helper function to take HTML response object and product object contains latitude and longitude
-    def process_gps_coordinates(self, response):
-        # produce string in format 'XXX.XXX,XXX.XXX'
-        lat_long_markup = response.css('#rspCol800 div.rspCol table tr:nth-child(2) td').extract()[1]
-        lat_long_string = re.search(r"q=[^&]*", lat_long_markup).group(0)[2:]
-        return {
-            'latitude' : lat_long_string.split()[0],
-            'longitude' : lat_long_string.split()[1]
-        }
+        try:
+            yield {
+                'area_name': area_name,
+                #'parents': response.css('#navBox div ::text').extract(),
+                #'child_areas': response.css('#viewerLeftNavColContent a[target="_top"] ::text').extract(),
+                'latitude' : lat_long_string.split(',')[0],
+                'longitude' : lat_long_string.split(',')[1]
+            }
+        except IndexError:
+            pass
+        links = response.css('#viewerLeftNavColContent a[target="_top"] ::attr(href)').extract()
+        for url in links:
+            yield scrapy.Request(self.domain + url, callback=self.parse_coordinates)
 
-
+    def process_latitude_longitude(self,response):
+        if("Location" not in response.css('#rspCol800 div.rspCol table tr:nth-child(2) td ::text').extract()[0]):
+            return response.css('#rspCol800 div.rspCol table tr:nth-child(3) td ::text').extract()[1].strip()
+        else:
+            return response.css('#rspCol800 div.rspCol table tr:nth-child(2) td ::text').extract()[1].strip()
