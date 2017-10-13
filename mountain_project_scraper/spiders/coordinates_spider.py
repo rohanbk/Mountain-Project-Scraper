07aaf2c20988ec3a677bc41119a27a1c0b51f1d8
Rohan Balakrishnan <rohanbk@gmail.com>
2017-10-12 20:25:03 -0700

    Remove weird characters/numbers from area names, and create parents (breadcrumb) information lambda

--- mountain_project_scraper/spiders/coordinates_spider.py
+++ mountain_project_scraper/spiders/coordinates_spider.py
@@ -7,7 +7,7 @@ from scrapy.spiders import CrawlSpider, Rule
 class CoordinatesSpider(scrapy.Spider):
     name = 'coordinates'
     domain = 'https://www.mountainproject.com'
-    start_urls = [domain + '/v/washington/105708966'] #replace this with mountain project homepage
+    start_urls = [domain + '/v/hawaii/106316122']
     allowed_domains = ['mountainproject.com']
     rules = [
         Rule(
@@ -23,14 +23,14 @@ class CoordinatesSpider(scrapy.Spider):
             yield scrapy.Request(self.domain + url, callback=self.parse_coordinates)
 
     def parse_coordinates(self, response):
-        area_name = response.css('h1.dkorange em ::text').extract_first().strip()
+        area_name = re.sub(r'\([A-Za-z0-9\.]+\)', '', response.css('h1.dkorange em ::text').extract_first()).strip()
         lat_long_string = self.process_latitude_longitude(response)
 
         try:
             yield {
                 'area_name': area_name,
-                #'parents': response.css('#navBox div ::text').extract(),
-                #'child_areas': response.css('#viewerLeftNavColContent a[target="_top"] ::text').extract(),
+                'parents': list(filter(lambda x: x != ' > ' and x != '\n\t\t\t\t', response.css('#navBox div ::text').extract())),
+                'child_areas': response.css('#viewerLeftNavColContent a[target="_top"] ::text').extract(),
                 'latitude' : lat_long_string.split(',')[0],
                 'longitude' : lat_long_string.split(',')[1]
             }
@@ -41,7 +41,7 @@ class CoordinatesSpider(scrapy.Spider):
             yield scrapy.Request(self.domain + url, callback=self.parse_coordinates)
 
     def process_latitude_longitude(self,response):
-        if("Location" not in response.css('#rspCol800 div.rspCol table tr:nth-child(2) td ::text').extract()[0]):
+        if "Location" not in response.css('#rspCol800 div.rspCol table tr:nth-child(2) td ::text').extract()[0]:
             return response.css('#rspCol800 div.rspCol table tr:nth-child(3) td ::text').extract()[1].strip()
         else:
             return response.css('#rspCol800 div.rspCol table tr:nth-child(2) td ::text').extract()[1].strip()
